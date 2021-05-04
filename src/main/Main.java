b85f2f292c80d53d798fe7314a7b93ff35966204
YoungSeok Hong <cevinlion@gmail.com>
2021-05-04 21:56:45 +0900

    <modify> 룰에 대한 설정

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		CustomerListImpl customerList = new CustomerListImpl();
-		customerList.insert(new Customer("경기도 광명시", "5kso3", "김소철", "01071300465"));
+		customerList.add(new Customer("경기도 광명시", "5kso3", "김소철", "01071300465"));
 		System.out.println(customerList.select("5kso3").getName());
 		customerList.updateNameById("5kso3", "김민태");
 		System.out.println(customerList.delete("majormin"));
