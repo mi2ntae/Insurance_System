6dbb3d718a31a93dd761149139552d1f089d743b
YoungSeokHong <80624082+YoungSeokHong@users.noreply.github.com>
2021-06-14 14:28:57 +0900

    .

--- src/db/DBConnector.java
+++ src/db/DBConnector.java
@@ -9,7 +9,7 @@ public class DBConnector {
 	
 	public void startDB() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
